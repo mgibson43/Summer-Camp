9a924e9319d2b3f335a0b5f8e6780b39b0870646
Matthew Gibson <mgib278@wgu.edu>
2024-08-06 19:46:17 -0400

    Update: Added sidebar and media queries for mobile friendliness

--- public/index.js
+++ public/index.js
@@ -1,2 +1,10 @@
 'use strict'
+const sidebar = document.querySelector('.sidebar');
 
+function showSidebar() {
+  sidebar.style.display = 'flex';
+};
+
+function closeSidebar() {
+  sidebar.style.display = 'none';
+}
